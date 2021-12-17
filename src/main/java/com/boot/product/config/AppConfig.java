ef31b92f8986f128ea40bb1fe0f970dee86064fc
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-12-17 12:16:27 +0200

    CORS filter removed

--- src/main/java/com/boot/product/config/AppConfig.java
+++ src/main/java/com/boot/product/config/AppConfig.java
@@ -3,30 +3,11 @@ package com.boot.product.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import com.boot.product.client.UserServiceClient;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
 
 	@Bean
 	public RestTemplate template() {
