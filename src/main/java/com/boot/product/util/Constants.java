d75d517a0b72260666963a6b1e45facf7043aee3
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-03-25 09:50:26 +0200

    Update Constants.java

--- src/main/java/com/boot/product/util/Constants.java
+++ src/main/java/com/boot/product/util/Constants.java
@@ -2,7 +2,8 @@ package com.boot.product.util;
 
 public class Constants {
 
-	public static final String BASE_URL = "http://localhost:8080/";
+	//public static final String BASE_URL = "http://localhost:8080/";
+	public static final String BASE_URL = "https://spring-store-user-service.herokuapp.com/";
 	public static final String GET_ALL_USERS = BASE_URL + "getAllUsers";
 	public static final String UPDATE_USER = BASE_URL + "updateUserByEmail/";
 }
