17f6df220235e451ba1c317ee1cef0d3446619d9
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2023-12-05 10:41:38 +0200

    Update ProductDTO.java

--- src/main/java/com/boot/product/dto/ProductDTO.java
+++ src/main/java/com/boot/product/dto/ProductDTO.java
@@ -11,6 +11,7 @@ import lombok.experimental.Accessors;
 import javax.persistence.Column;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -49,6 +50,7 @@ public class ProductDTO {
 	@Positive(message = "Product stock should be positive number!")
 	private int stock;
 
+	@Valid
 	@NotNull
 	private CharacteristicsDTO characteristics;
 
